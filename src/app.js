c02959057dad26fee5a471e188d56e21806e0835
emewarp <martawarp@gmail.com>
2021-12-13 16:59:43 +0000

    base template for class

--- src/app.js
+++ src/app.js
@@ -8,4 +8,18 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  giveMeThePassword();
 };
+
+function giveMeThePassword(student, house) {
+  const houses = ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"];
+
+  const housesAndPasswords = [
+    { house: "Gryffindor", password: "a" },
+    { house: "Slytherin", password: "b" },
+    { house: "Ravenclaw", password: "c" },
+    { house: "Hufflepuff", password: "d" }
+  ];
+
+  document.getElementById("heading").innerHTML = "Welcome to Hogwarts";
+}
